16e07a5806d56df552ba9e273b58ee7fbf9dfa26
Iconoclast <cxd4@uniplex.cat-vs-internet.gov>
2018-10-04 02:04:02 -0400

    added separate page for playing from user-specified file

---
+++ play.js
@@ -0,0 +1,38 @@
+var PGN_file;
+
+function JS_main(ML_interface) {
+    "use strict";
+    var offset;
+    var limit;
+    var needle = "f=";
+    var href = ML_interface.location.href;
+
+    offset = href.search(needle);
+    if (offset >= 0) {
+        limit = href.length; /* To do:  Adjust if '&' occurs. */
+        PGN_file = href.substring(offset + needle.length, limit);
+    }
+
+new PgnViewer( {
+    boardName                      :  "games",
+    pgnFile                        :  PGN_file,
+    pieceSet                       :  "merida",
+    pieceSize                      :  55,
+    movesFormat                    :  "default",
+    pauseBetweenMoves              :  1000,
+
+ // pollPGNMilliseconds            :  5000,
+ // gotoEndOnRefresh               :  true,
+    highlightFromTo                :  true,
+    showCoordinates                :  true,
+    autoScrollMoves                :  true,
+ // showBracketsOnVariation        :  false,
+    hideBracketsOnTopLevelVariation:  true,
+    moveAnimationLength            :  0.3,
+    newlineForEachMainMove         :  false,
+ // variationStartString           :  "[",
+ // variationEndString             :  "]",
+} );
+    return;
+}
+
